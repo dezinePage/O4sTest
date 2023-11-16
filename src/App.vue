2393ea79eaccedc4a814b373646ed254ce774f5b
subhraO4s <subhrajyoti.baroi@original4sure.com>
2023-11-16 10:51:24 +0530

    Initial Commit.

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <HelloWorld msg="You did it!" />
+  </header>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    justify-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    justify-items: center;
+  }
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { UAParser } from 'ua-parser-js'
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+const plat = window.navigator.platform
+let OSName = 'Unknown OS'
+let parser = new UAParser()
+console.log(parser.getResult())
+</script>
+
+<template>
+  <div class="greetings">
+    <h3>Browser - {{ parser.getBrowser().name }}</h3>
+    <h3>Browser Version - {{ parser.getBrowser().version }}</h3>
+    <h3>OS(library) - {{ parser.getOS().name }}</h3>
+    <h3>Platform - {{ plat }}</h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
